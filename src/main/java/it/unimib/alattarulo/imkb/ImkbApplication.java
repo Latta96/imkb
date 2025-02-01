a81c4651a0f78e303a111543a89dcc325064aed5
Andrea Lattarulo <lattarulo.andrea@proton.me>
2025-02-01 09:21:00 +0100

    first commit

---
+++ src/main/java/it/unimib/alattarulo/imkb/ImkbApplication.java
@@ -0,0 +1,13 @@
+package it.unimib.alattarulo.imkb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImkbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImkbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unimib/alattarulo/imkb/ImkbApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unimib.alattarulo.imkb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImkbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
